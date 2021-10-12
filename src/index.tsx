8dc15e8c77196c4ff32b0fd819801acddcea8c37
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-10-12 16:42:15 -0700

    Create functional counter

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 interface AppProps {
   color?: string;
 }
 
-const App = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>;
-};
+// const App = (props: AppProps): JSX.Element => {
+//   const [count, setCount] = useState(0);
 
-// class App extends React.Component<AppProps> {
-//   state = { counter: 0 };
-
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
+//   const onIncrement = (): void => {
+//     setCount(count + 1);
 //   };
 
-//   onDecrement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
+//   const onDecrement = (): void => {
+//     setCount(count - 1);
 //   };
 
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
+//   return (
+//     <div>
+//       <button onClick={onIncrement}>increment</button>
+//       <button onClick={onDecrement}>decrement</button>
+//       {count}
+//     </div>
+//   );
+// };
+
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App color="red" />, document.querySelector('#root'));
