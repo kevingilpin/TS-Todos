dbe45ba7d621155dff507d62be1554e9b167c136
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-10-12 19:32:14 -0700

    Refactor action types

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,6 +1,6 @@
-import { FetchTodosAction, Todo, ActionTypes } from '../actions';
+import { Action, Todo, ActionTypes } from '../actions';
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
